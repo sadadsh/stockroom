d008946c82228910f0c781407c14c56bd772c330
Sadad Haidari <sadad.haidari@caci.com>
2026-07-12 20:44:32 -0400

    Add kicad-cli wrapper for version, symbol upgrade, and SVG export

---
+++ app/backend/stockroom/kicad/__init__.py
@@ -0,0 +1,4 @@
+from stockroom.kicad.cli import KiCadCli
+from stockroom.kicad.errors import KiCadCliError, KiCadError, KiCadFileError
+
+__all__ = ["KiCadCli", "KiCadError", "KiCadFileError", "KiCadCliError"]
